1ea9da2b34daeea48f007c8e815d750817a168cf
Matt Bhagat-Conway <matt@indicatrix.org>
2026-02-21 11:30:16 -0500

    code readability cleanup

--- R/diagon_alley.R
+++ R/diagon_alley.R
@@ -1,7 +1,12 @@
 diagon_alley = function (size, target_x, target_y) {
     gridded_graph(size, size) +
         # add the diagonal edge
-        edge(get_grid_vid(target_x - 1, target_y - 1, size), get_grid_vid(target_x + 1, target_y + 1, size), weight = 2 * sqrt(2), curvature=0) -
+        edge(
+            get_grid_vid(target_x - 1, target_y - 1, size),
+            get_grid_vid(target_x + 1, target_y + 1, size),
+            weight = 2 * sqrt(2),
+            curvature=0
+        ) -
         # delete the central vertex
         vertex(get_grid_vid(target_x, target_y, size))
 }
