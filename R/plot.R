2836f42298f20637fd3297e2083fc03f700dc707
Matt Bhagat-Conway <matt@indicatrix.org>
2026-02-20 20:29:39 -0500

    Clean up plot code

--- R/plot.R
+++ R/plot.R
@@ -1,21 +1,21 @@
+#' Plot a graph, optionally adjusting colors or line widths based on
+#' some vector of edge attributes.
 #' @export
-plot_graph = function (G, value=NULL) {
-    vertices = tibble(
-        vid = vertex_attr(G, "name"),
-        x = vertex_attr(G, "x"),
-        y = vertex_attr(G, "y")
-    )
-
-    edges = as_tibble(as_edgelist(G))
-    names(edges) = c("vid_start", "vid_end")
+plot_graph = function (G, value=NULL, linewidth=NULL) {
+    vertices = igraph::as_data_frame(G, what="vertices")
+    edges = igraph::as_data_frame(G, what="edges") |>
+        rename(name_start="from", name_end="to")
     edges$value = value
+    edges$linewidth = linewidth
     edges$curvature = edge_attr(G, "curvature")
     edges = edges |>
-        left_join(rename_with(vertices, \(x) glue("{x}_start")), by="vid_start") |>
-        left_join(rename_with(vertices, \(x) glue("{x}_end")), by="vid_end") 
+        left_join(rename_with(vertices, \(x) glue("{x}_start")), by="name_start") |>
+        left_join(rename_with(vertices, \(x) glue("{x}_end")), by="name_end") 
     
     if (!is.null(value)) {
         p = ggplot() + lapply(1:nrow(edges), \(i) geom_curve(aes(x=x_start, y=y_start, xend=x_end, yend=y_end, color=value), data=edges[i,], curvature=edges$curvature[[i]], linewidth=3, lineend="round"))
+    } else if (!is.null(linewidth)) {
+        p = ggplot() + lapply(1:nrow(edges), \(i) geom_curve(aes(x=x_start, y=y_start, xend=x_end, yend=y_end, linewidth=linewidth, alpha=linewidth), data=edges[i,], curvature=edges$curvature[[i]], lineend="round"))
     } else {
         # no color and legend
         p = ggplot() + lapply(1:nrow(edges), \(i) geom_curve(aes(x=x_start, y=y_start, xend=x_end, yend=y_end), data=edges[i,], curvature=edges$curvature[[i]], color="gray", linewidth=3, lineend="round"))
